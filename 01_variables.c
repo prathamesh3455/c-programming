81f7254c53683cd8beb37044f29d8ca47c0686bd
Prathamesh Adhav <155577783+prathamesh3455@users.noreply.github.com>
2024-10-26 21:12:57 +0530

    Add files via upload

---
+++ 01_variables.c
@@ -0,0 +1,15 @@
+/* date= 26-10-2024
+chapter 01
+variable code */
+
+#include<stdio.h>
+
+int main(){
+  int a;
+  a = 6;
+  //int a = 6; This is also valid 
+
+  printf("The Output of variable is %d",a);
+  return 0;
+  
+}
